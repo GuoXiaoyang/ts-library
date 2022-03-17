9104578398416703ad4b46ffa1c689c433c706d1
guoxiaoyang <guoxiaoyang0925@gmail.com>
2022-03-17 20:30:51 +0800

    feat(wip): curry

---
+++ src/curry/curry.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: guoxiaoyang <xiaoyang@moego.pet>
+ * @Date: 2022-03-17 20:29:29
+ * @LastEditTime: 2022-03-17 20:30:32
+ * @LastEditors: guoxiaoyang
+ * @FilePath: /ts-library/src/curry/curry.ts
+ */
+
+/**
+ * @reference [document](https://github.com/mqyqingfeng/Blog/issues/42)
+ * @param func 
+ * @returns 
+ */
+function curry(func: Function): Function {
+  return () => {
+
+  }
+}
