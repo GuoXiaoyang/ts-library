0166c2ab1f5ab6183529f970c51000888694dba3
guoxiaoyang <guoxiaoyang0925@gmail.com>
2022-04-24 11:09:52 +0800

    feat(wip): add hole version for curry

--- src/curry/curry.test.ts
+++ src/curry/curry.test.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: guoxiaoyang <xiaoyang@moego.pet>
  * @Date: 2022-03-20 20:35:44
- * @LastEditTime: 2022-03-20 21:55:06
+ * @LastEditTime: 2022-03-21 23:15:36
  * @LastEditors: guoxiaoyang
  * @FilePath: /ts-library/src/curry/curry.test.ts
  */
 
-import { curryWithoutHoles, curryWithoutHoles2 } from "./curry";
+import { curryWithHoles, curryWithoutHoles, curryWithoutHoles2 } from "./curry";
 
 const testFunc: (...args: number[]) => number[] = (a, b, c, d, e) => {
   return [a, b, c, d, e];
@@ -18,14 +18,14 @@ describe("Curry without hole", () => {
     expect(curriedFunc(1, 2, 3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test('should work correctly when called twice', () => {
+  test("should work correctly when called twice", () => {
     const curriedFunc = curryWithoutHoles(testFunc);
     expect(curriedFunc(1, 2)(3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
-  })
-  test('should work correctly when initial with one and called twice', () => {
+  });
+  test("should work correctly when initial with one and called twice", () => {
     const curriedFunc = curryWithoutHoles(testFunc, 1);
     expect(curriedFunc(2)(3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
-  })
+  });
 });
 
 describe("Curry without hole version2", () => {
@@ -34,8 +34,40 @@ describe("Curry without hole version2", () => {
     expect(curriedFunc(1, 2, 3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test('should work correctly when called twice', () => {
+  test("should work correctly when called twice", () => {
     const curriedFunc = curryWithoutHoles2(testFunc);
     expect(curriedFunc(1, 2)(3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
+  });
+});
+
+
+describe("Curry with hole version", () => {
+  test('should work correctly when called once', () => {
+    const curriedFunc = curryWithHoles(testFunc);
+    expect(curriedFunc(1, 2, 3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
   })
+  // test('should work correctly when called with one hole', () => {
+  //   const curriedFunc = curryWithHoles(testFunc);
+  //   expect(curriedFunc('_', 2, 3, 4, 5)(1)).toEqual([1, 2, 3, 4, 5]);
+  // })
+
+  // test("should work correctly when called with one hole at index 2", () => {
+  //   const curriedFunc = curryWithHoles(testFunc);
+  //   expect(curriedFunc(1, "_", 3, 4, 5)(2)).toEqual([1, 2, 3, 4, 5]);
+  // });
+
+  // test("should work correctly when called twice with two holes", () => {
+  //   const curriedFunc = curryWithHoles(testFunc);
+  //   expect(curriedFunc(1, "_", 3)("_", 4)(2)(5)).toEqual([1, 2, 3, 4, 5]);
+  // });
+
+  // test("should work correctly when called 3 times", () => {
+  //   const curriedFunc = curryWithHoles(testFunc);
+  //   expect(curriedFunc(1, "_", "_", 4)("_", 3)(2)(5)).toEqual([1, 2, 3, 4, 5]);
+  // });
+
+  // test("should work correctly when called 4 times", () => {
+  //   const curriedFunc = curryWithHoles(testFunc);
+  //   expect(curriedFunc("_", 2)("_", "_", 4)(1)(3)(5)).toEqual([1, 2, 3, 4, 5]);
+  // });
 });
--- src/curry/curry.ts
+++ src/curry/curry.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: guoxiaoyang <xiaoyang@moego.pet>
  * @Date: 2022-03-17 20:29:29
- * @LastEditTime: 2022-03-20 21:54:40
+ * @LastEditTime: 2022-04-24 11:08:31
  * @LastEditors: guoxiaoyang
  * @FilePath: /ts-library/src/curry/curry.ts
  */
@@ -11,7 +11,10 @@
  * @param func
  * @returns
  */
-export function curryWithoutHoles(func: Function, ...argsHaveBeenCalled: any[]): Function {
+export function curryWithoutHoles(
+  func: Function,
+  ...argsHaveBeenCalled: any[]
+): Function {
   return function (...args: any[]) {
     const allArgs = argsHaveBeenCalled.concat(...args);
     if (allArgs.length >= func.length) {
@@ -32,9 +35,49 @@ export const curryWithoutHoles2 = <T>(fn: Function) => {
   };
 };
 
-
-export function curryWithHoles<T>(fn:Function, ...argsHaveBeenCalled: T[]){
-  return function (...args: Array<T | '_'>) {
-    
-  }
+const PlaceHolder = "_";
+/**
+ *
+ * @param fn function need to be curried
+ * @param argsHaveBeenCalled all params have been called, holes included
+ * FIXME: haven't passed the test cases till now
+ * @returns
+ */
+export function curryWithHoles<T>(
+  fn: Function,
+  ...argsHaveBeenCalled: Array<T | typeof PlaceHolder>
+) {
+  return function (...args: Array<T | typeof PlaceHolder>) {
+    const allArgs: Array<T | typeof PlaceHolder> = [...argsHaveBeenCalled];
+    let placeHolderLenInCalled = argsHaveBeenCalled.reduce(
+      (sum, arg) => (arg === PlaceHolder ? (sum += 1) : sum),
+      0
+    );
+    let placeHolderLen = 0;
+    for (const arg of args) {
+      if (arg === PlaceHolder) {
+        placeHolderLen += 1;
+        if (placeHolderLen > placeHolderLenInCalled) {
+          allArgs.push(PlaceHolder);
+        } else {
+          
+        }
+      } else {
+        if (argsHaveBeenCalled.includes(PlaceHolder)) {
+          const placeHolderIndex = argsHaveBeenCalled.indexOf(PlaceHolder);
+          allArgs.splice(placeHolderIndex, 1, arg);
+          argsHaveBeenCalled.splice(placeHolderIndex, 1);
+          if (placeHolderLenInCalled) {
+            placeHolderLenInCalled -= 1;
+          }
+        } else {
+          allArgs.push(arg);
+        }
+      }
+    }
+    if (allArgs.length >= fn.length && !allArgs.includes(PlaceHolder)) {
+      return fn.call(this, ...allArgs);
+    }
+    return curryWithHoles.call(this, fn, ...allArgs);
+  };
 }
